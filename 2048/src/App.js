7d220aaad63ade11d3ece2e66d75135ce2b009e9
Sunnys <sunnys@opentext.com>
2021-10-29 20:28:38 -0400

    debug setup and block format

--- 2048/src/App.js
+++ 2048/src/App.js
@@ -3,24 +3,76 @@ import Block from "./Blocks/Block.js";
 import React, { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState(0);
+  const [valueRow, setValueRow] = useState([
+    [2, 2, 2, 2],
+    [0, 0, 4, 0],
+    [0, 4, 3, 0],
+    [0, 0, 0, 0],
+  ]);
+
+  function moveBlock(row) {
+    for (let i = 1; i < 3; i++) {
+      if (row[i - 1] == 0) {
+        row[i - 1] = row[i];
+        row[i] = 0;
+        console.log(row);
+        moveBlock(row);
+      } else {
+      }
+    }
+  }
+
   document.onkeydown = function (e) {
     switch (e.keyCode) {
       case 37:
-        alert("left");
-        setValue(value + 1);
+        let newValueLeft = [];
+
+        valueRow.map((row) => {
+          for (let i = 0; i < row.length - 1; i++) {
+            if (row[i] == row[i + 1]) {
+              row[i] *= 2;
+              row[i + 1] = 0;
+              break;
+            }
+          }
+
+          // moveBlock(row);
+
+          newValueLeft.push(row);
+        });
+
+        console.log(newValueLeft);
+        setValueRow(newValueLeft);
+
         break;
       case 38:
         alert("up");
-        setValue(value + 1);
+        setValueRow(valueRow + 1);
         break;
       case 39:
-        alert("right");
-        setValue(value + 1);
+        let newValueRight = [];
+
+        valueRow.map((row) => {
+          row.sort(function (a, b) {
+            return a - b;
+          });
+          for (let i = 0; i < row.length - 1; i++) {
+            if (row[i] == row[i + 1]) {
+              row[i] = 0;
+              row[i + 1] *= 2;
+            }
+          }
+          newValueRight.push(row);
+          console.log(row);
+        });
+
+        console.log(newValueRight);
+        setValueRow(newValueRight);
+
         break;
       case 40:
         alert("down");
-        setValue(value + 1);
+        setValueRow(valueRow + 1);
         break;
     }
   };
@@ -30,28 +82,28 @@ function App() {
         <div class="backLayer">
           <div class="frontLayer">
             <div class="blockRow">
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
+              <Block valueRow={valueRow[0][0]}></Block>
+              <Block valueRow={valueRow[0][1]}></Block>
+              <Block valueRow={valueRow[0][2]}></Block>
+              <Block valueRow={valueRow[0][3]}></Block>
             </div>
             <div class="blockRow">
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
+              <Block valueRow={valueRow[1][0]}></Block>
+              <Block valueRow={valueRow[1][1]}></Block>
+              <Block valueRow={valueRow[1][2]}></Block>
+              <Block valueRow={valueRow[1][3]}></Block>
             </div>
             <div class="blockRow">
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
+              <Block valueRow={valueRow[2][0]}></Block>
+              <Block valueRow={valueRow[2][1]}></Block>
+              <Block valueRow={valueRow[2][2]}></Block>
+              <Block valueRow={valueRow[2][3]}></Block>
             </div>
             <div class="blockRow">
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
-              <Block value={value}></Block>
+              <Block valueRow={valueRow[3][0]}></Block>
+              <Block valueRow={valueRow[3][1]}></Block>
+              <Block valueRow={valueRow[3][2]}></Block>
+              <Block valueRow={valueRow[3][3]}></Block>
             </div>
           </div>
           <div class="buttonGroup">
--- 2048/src/Blocks/Block.js
+++ 2048/src/Blocks/Block.js
@@ -1,5 +1,5 @@
 function Block(props) {
-  return <div class="block">{props.value}</div>;
+  return <div class="block">{props.valueRow}</div>;
 }
 
 export default Block;
